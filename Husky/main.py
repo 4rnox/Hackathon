b7deb9d811e51e3e877a16d86174bc819e6bff49
aGoose <75203757+PauGalopaDev@users.noreply.github.com>
2021-05-15 16:06:21 +0200

    _

--- Husky/main.py
+++ Husky/main.py
@@ -1,10 +1,23 @@
+import hashlib
 import time
 from flask import Flask
+import hashlib
 
 app = Flask(__name__)
 
-@app.route('/lets')
-def get_current_time():
-    print("Lets Gooooo!!")
 
+@app.route('/register/<userdata>')
+def register_user(userdata):
 
+    hashlib.sha512(userdata["password"]).hexdigest()
+    # Hash de usuari i contra
+    # SQLquery -- return 1 for succes, -1 for error
+    # 
+
+"""
+{
+    "user" : "",
+    "password" : ""
+}
+
+"""
---
+++ Husky/test.py
@@ -0,0 +1,17 @@
+import hashlib
+
+userdata = {}
+
+
+hashlib.sha512(userdata["password"]).hexdigest()
+    # Hash de usuari i contra
+    # SQLquery -- return 1 for succes, -1 for error
+    # 
+
+"""
+{
+    "user" : "",
+    "password" : ""
+}
+
+"""
